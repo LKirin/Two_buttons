c9f0e087db4f91ea913db2a2e62f6a05914a58da
Воронцов Кирилл Алексеевич <vorontcovka.27@sch239.net>
2024-05-17 20:32:18 +0300

    labirint has made

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,57 @@
 #include <string>
-#include<iostream>
+#include <iostream>
 #include <vector>
 #include <raylib.h>
 #include "block.hpp"
 
 using namespace std;
 
-void creatingMap(vector<vector<Block>> &playMap, int size, Texture2D texWall, Texture2D texFloor){//25 * 21 map
+void creatingMap(vector<vector<Block>> &playMap, int size, Texture2D texWall, Texture2D texFloor, Image schema)
+{ // 25 * 21 map
     int width = 25 * size;
     int height = 21 * size;
-
     for (int i = 0; i < height / size; i++)
     {
-        vector<Block> emptyVec; 
+        vector<Block> emptyVec;
         playMap.push_back(emptyVec);
     }
-    
-    //first level---------------------------------------------
-    for(int i = 0; i < width / size; i++){
-        if(i == 0 || i == 15 || i == 17 || i == 24){
-            Block f(texFloor, {float(i * size), 0.f}, {size, size});
-            playMap[0].push_back(f);            
-        }
-        else{
-            Block w(texWall, {float(i * size), 0.f}, {size, size});
-            playMap[0].push_back(w);
+
+    for (int i = 0; i < 21; i++)
+    {   
+        for (int j = 0; j < 25; j++)
+        {
+            if (GetImageColor(schema, j, i).r == 0)
+            {
+                Block w(texWall, {float(j * size), float(i * size)}, {float(size), float(size)});
+                playMap[i].push_back(w);
+            }
+            else
+            {
+                Block f(texFloor, {float(j * size), float(i * size)}, {float(size), float(size)});
+                playMap[i].push_back(f);
+            }
         }
     }
-    //----------------------------------------------------------
+
 }
 
 int main()
-{   
+{
 
     Color warmColor = {179, 148, 74, 255};
-    Vector2 size ={50, 50};
+    Vector2 size = {50, 50};
 
-    
-
-    const int screenWidth = 1260;
-    const int screenHeight = 1060;
+    const int screenWidth = 1250;
+    const int screenHeight = 1050;
 
     InitWindow(screenWidth, screenHeight, "Cool game");
 
     Texture2D wall = LoadTexture("../wallTexture.png");
     Texture2D floor = LoadTexture("../floorTexture.png");
+    Image schema = LoadImage("../LabirintSchema.png");
+
     vector<vector<Block>> mapPlaying;
-    creatingMap(mapPlaying, size.x, wall, floor);
+    creatingMap(mapPlaying, size.x, wall, floor, schema);
 
     SetTargetFPS(60);
 
@@ -54,12 +59,14 @@ int main()
     {
         BeginDrawing();
         ClearBackground(WHITE);
-        for(int i = 0; i < mapPlaying.size(); i++){
-            for(int j = 0; j < mapPlaying[i].size(); j++){
-                DrawTextureEx(mapPlaying[i][j].texture, mapPlaying[i][j].pos, 0.f, 1, WHITE);
+        for (int i = 0; i < mapPlaying.size(); i++)
+        {
+            for (int j = 0; j < mapPlaying[i].size(); j++)
+            {
+                DrawTextureEx(mapPlaying[i][j].texture, mapPlaying[i][j].pos, 0.f, 0.05, WHITE);
             }
         }
-            
+
         EndDrawing();
     }
     CloseWindow();
